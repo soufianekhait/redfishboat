81cdfe61c55df376837557d4718b06ac8827e53e
Soufiane KHAIT <45060816+soufianekhait@users.noreply.github.com>
2019-01-09 11:30:48 +0100

    Add gallery, fonts & images

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    $(".loading").fadeOut(3000);
+
+    $(".loading").fadeOut(3000, function(){
+        console.log("enter");
+        $("body").css('overflow', 'auto');
+    });
+
+    var mySwiper = new Swiper(".swiper-container", {
+        direction: "vertical",
+        loop: true,
+        pagination: ".swiper-pagination",
+        grabCursor: true,
+        speed: 1000,
+        paginationClickable: true,
+        parallax: true,
+        autoplay: false,
+        effect: "slide",
+        mousewheelControl: 1
+      });      
 });
